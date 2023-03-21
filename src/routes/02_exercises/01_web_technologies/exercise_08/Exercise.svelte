d4bbf2d3b04d1f341681a0748f02088679885539
yasemin98 <yasemin.us98@gmail.com>
2023-03-21 17:22:59 +0100

    Update exercise 8

---
+++ src/routes/02_exercises/01_web_technologies/exercise_08/Exercise.svelte
@@ -0,0 +1,8 @@
+<script>
+    const me = {
+        name: 'Yasemin Uslu',
+        things: ['libraries', 'coffee dates', 'thrifting']
+    };
+</script>
+
+<p>My name is {me.name} and these are some of my favourite things: {me.things}. </p>
