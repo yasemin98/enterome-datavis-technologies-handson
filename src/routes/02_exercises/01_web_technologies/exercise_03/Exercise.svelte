028c117680f46c12e26162563a78c38ac89d6763
yasemin98 <yasemin.us98@gmail.com>
2023-04-17 16:03:23 +0200

    Update exercises

--- src/routes/02_exercises/01_web_technologies/exercise_03/Exercise.svelte
+++
@@ -1,6 +0,0 @@
-<p> The best visual channels ordered by Mackinlay's ranking:</p>
-<ol>
-    <li> position</li>
-    <li> length</li>
-    <li> angle</li>
-</ol>
---
+++ src/routes/02_exercises/01_web_technologies/exercise_10/Exercise.svelte
@@ -0,0 +1,9 @@
+<script> 
+    import Ex3 from '../exercise_03/Exercise.svelte';
+    import Ex2 from '../exercise_02/Exercise.svelte';
+</script>
+
+<div> 
+    <Ex3 />
+    <Ex2 />
+</div>
