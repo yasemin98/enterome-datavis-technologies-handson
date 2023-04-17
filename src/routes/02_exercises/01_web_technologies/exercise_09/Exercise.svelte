2fbd809276ecacd288d47e482d24fa486e2e2e57
yasemin98 <yasemin.us98@gmail.com>
2023-04-17 16:02:55 +0200

    Update exercise 9

---
+++ src/routes/02_exercises/01_web_technologies/exercise_09/Exercise.svelte
@@ -0,0 +1,10 @@
+<script>
+    function circumference(radius) {
+      return 2 * Math.PI * radius;
+    }
+  
+    const radius = 4;
+    const circumferenceValue = circumference(radius);
+  </script>
+
+  <p> The circumference of a circle with radius {radius}m is {circumferenceValue.toFixed(2)}m. </p>
